df3d1ce155224c5b4c3567e8ddbcb87603141df6
Simond Li <62400087+Simoidne@users.noreply.github.com>
2024-07-22 10:21:30 +0100

    added GetAssignmentUseCase

--- src/main/java/use_case/GetAssignmentsUseCase.java
+++ src/main/java/use_case/GetAssignmentsUseCase.java
@@ -1,19 +1,37 @@
 package use_case;
 
-import ApiPackage.ChatbotAPI;
-import ApiPackage.UserDB;
-import ApiPackage.GeminiAPI;
 import ApiPackage.SyllabusConversion;
+import ApiPackage.RawSyllabus;
+import ApiPackage.SyllabusNotFoundException;
 import entity.Assignment;
 import entity.Course;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 public class GetAssignmentsUseCase {
-    public List<Assignment> getAssignments(UserDB userDB, Course course) {
-        List<Assignment> assignments = new ArrayList<>();
-        ChatbotAPI chatbotAPI = new GeminiAPI();
+    public List<Assignment> getAssignments(Course course) throws SyllabusNotFoundException {
+        if (course.getAssignments() != null) {
+            return course.getAssignments();
+        } else {
+            SyllabusConversion syllabusConverter = new SyllabusConversion();
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Syllabus: ");
+            String syllabusData = scanner.nextLine();
+            System.out.println("Enter data format used for syllabus (TEXT or HTML): ");
+            String syllabusFormat = scanner.nextLine();
+
+            if (!syllabusFormat.equals("HTML") && !syllabusFormat.equals("TEXT")) {
+                throw new IllegalArgumentException("Syllabus format is not supported");
+            }
+
+            RawSyllabus rawSyllabus = new RawSyllabus(syllabusFormat,
+                    syllabusData,
+                    course.getId(),
+                    true);
+            List<Assignment> assignments = syllabusConverter.getAssignments(rawSyllabus);
+            return assignments;
+        }
     }
 }
